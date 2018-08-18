b9cec92fa42dfd4e86d120e59c617f7d2efb01c0
Siddharth <xxxxxxxxxxxxxxx@xxx.xxx>
2018-08-19 01:57:52 +0530

    Filtering products complete.

--- src/pages/filter-modal/filter-modal.ts
+++ src/pages/filter-modal/filter-modal.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 
 @Component({
     selector: 'page-filter-modal',
     templateUrl: 'filter-modal.html',
 })
 export class FilterModalPage {
-    constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    maleSelected:boolean
+    femaleSelected: boolean
+    constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+        this.maleSelected = this.navParams.get('maleSelected')
+        this.femaleSelected = this.navParams.get('femaleSelected')
+    }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad FilterModalPage');
     }
 
     closeModal() {
-        this.navCtrl.pop();
+        let filterState = {
+            femaleSelected: this.femaleSelected,
+            maleSelected: this.maleSelected
+        }
+        
+        this.viewCtrl.dismiss(filterState)
     }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,9 @@ import { FilterModalPage } from '../filter-modal/filter-modal';
     templateUrl: 'home.html'
 })
 export class HomePage {
+    maleSelected:boolean = true
+    femaleSelected: boolean = true
     allProducts
-
     constructor(public navCtrl: NavController, private modalCtrl: ModalController, private _products: ProductProvider) { }
 
     ionViewDidLoad() {
@@ -20,7 +21,35 @@ export class HomePage {
     }
 
     openFilterModal() {
-        let modal = this.modalCtrl.create(FilterModalPage).present()
+        let filterStateMainPage = {
+            maleSelected: this.maleSelected,
+            femaleSelected: this.femaleSelected
+        }
+        let modal = this.modalCtrl.create(FilterModalPage, filterStateMainPage)
+
+        modal.onDidDismiss((filterState) => {
+            this.maleSelected = filterState.maleSelected
+            this.femaleSelected = filterState.femaleSelected
+            
+            this._products.getProducts().subscribe(
+                res => {
+                    let products = res
+                    
+                    if(filterState.maleSelected && filterState.femaleSelected) {
+                        this.allProducts = products
+                    } else if (!filterState.maleSelected && !filterState.femaleSelected) {
+                        this.allProducts = []
+                    } else if (filterState.maleSelected && !filterState.femaleSelected) {
+                        this.allProducts = res.filter(x => x.gender.toLowerCase() !== 'female')
+                    } else if (!filterState.maleSelected && filterState.femaleSelected){
+                        this.allProducts = res.filter(x => x.gender.toLowerCase() !== 'male')
+                    }
+                }
+            )
+        })
+
+        modal.present()
+        
     }
 
     goToProductDetailPage(product) {
